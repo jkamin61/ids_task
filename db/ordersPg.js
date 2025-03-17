76fa639ca705ac42d15036ec4abc06a8a65b994d
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2025-03-18 00:23:46 +0100

    launched pg on supabase

--- db/ordersPg.js
+++ db/ordersPg.js
@@ -1,39 +1,11 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-})
-
-const createTable = async () => {
-    const query = `
-        CREATE TABLE IF NOT EXISTS orders
-        (
-            orderID
-            TEXT
-            PRIMARY
-            KEY,
-            products
-            JSONB,
-            orderWorth
-            REAL,
-            orderDate
-            TIMESTAMP
-            DEFAULT
-            CURRENT_TIMESTAMP
-        );
-    `;
-    try {
-        await pool.query(query);
-    } catch (error) {
-        console.error("Error creating table:", error);
-    }
-};
-
-createTable();
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
+});
 
 module.exports = pool;
